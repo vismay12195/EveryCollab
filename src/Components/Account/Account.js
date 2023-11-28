d6f5b0cf871ceb0f5663181ee522c353c5d1a13e
vismay12195 <130983117+vismay12195@users.noreply.github.com>
2023-11-28 18:39:22 +0530

    Added the functionality to Camera Icon with firebase 'storage' connectivity

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from './Account.module.css';
 import { Camera, LogOut } from "react-feather";
 import InputControl from "../InputControl/InputControl";
 import { Navigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
-import { auth } from "../../firebase";
+import { auth, uploadImage } from "../../firebase";
 
 
 const Account = (props) => {
     const userDetails = props.userDetails;
     const loginAuthenticated = props.auth;
 
+    // Using the Reference hook for picking the image when clicked camera icon
+    const imagePicker = useRef();
+    // Image upload percentage progress state
+    const [progress, setProgress] = useState(0);
+    // Showing the profile image upload started or not for smaller profile image size
+    const [profileImageUploadStarted, setProfileImageUploadStarted] = useState(false);
+    // Upon complete profile image upload url state
+    const [profileImageURL, setProfileImageURL] = useState("https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png");
+
     // Logout function definition using the signOut functionality of Firestore
     const accountLogout = async () => {
         await signOut(auth);
+    };
+
+    // Camera Click function to add the profile image for the user account
+    const cameraClick = () => {
+        imagePicker.current.click();
     }
+
+    // Upon Selecting the image, it should change the image
+    const imageChange = (event) => {
+        // selecting the image file
+        const file = event.target.files[0];
+
+        if (!file) return;
+
+        setProfileImageUploadStarted(true);
+        // Calling the uploadImage function with its parameters from the firebase
+        uploadImage(file,
+            (progress) => setProgress(progress),
+            (url) => {
+                setProfileImageURL(url);
+                setProfileImageUploadStarted(false);
+                setProgress(0);
+            },
+            (err) => {
+                console.log('Error ->', err);
+                setProfileImageUploadStarted(false);
+            }
+        );
+
+    };
+
     // Account verification would be implemented first
     return loginAuthenticated ? (
         <>
@@ -31,17 +70,29 @@ const Account = (props) => {
                     </div>
                 </div>
 
+                {/* Adding the input field to the camera icon to add images from local computer with onClick event */}
+                <input ref={imagePicker} type="file" style={{ display: "none" }} onChange={imageChange} />
                 {/* -------------- # Section Part --------------   */}
                 <div className={styles.section}>
                     <div className={styles.title}>Your Profile</div>
                     <div className={styles.profile}>
                         <div className={styles.left}>
                             <div className={styles.image}>
-                                <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" alt="Profile Image"></img>
-                                <button className={styles.camera}>
+                                {/* Dynamically rendering the profile Image */}
+                                <img src={profileImageURL} alt="Profile Image"></img>
+                                <div className={styles.camera} onClick={cameraClick} >
                                     <Camera />
-                                </button>
+                                </div>
                             </div>
+                            {
+                                profileImageUploadStarted ? (
+                                    <p className={styles.progress}>
+                                        {/* Adding a constraint if the image takes too long to upload */}
+                                        {
+                                            (progress === 100) ? "Getting image" : `${progress.toFixed(2)}% uploaded`}</p>
+                                )
+                                    : ("")
+                            }
                         </div>
                         <div className={styles.right}>
                             <div className={styles.row}>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions of Firebase SDK
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore'
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDehwb1NUpsKE_I9o1t2YHZK2fxz1Kbn_w",
@@ -22,6 +23,10 @@ const auth = getAuth(app);
 
 // Creating an object for Cloud Firestore database
 const db = getFirestore(app);
+
+// Creating an object to store the user profile pic to the Firebase's storage
+const storage = getStorage(app);
+
 //Function to add the users data to the database collection, uid is coming from the promise values user -> uid
 const updateUserDatabase = async (user, uid) => {
     // If the user data is not in the form of object then just return
@@ -43,5 +48,57 @@ const getUserFromDatabase = async (uid) => {
     return result.data();
 }
 
+// Adding the User Profile images to the Firebase Storage
 
-export { app as default, auth, db, updateUserDatabase, getUserFromDatabase };
+const uploadImage = (file, progressCallback, urlCallback, errorCallback) => {
+    if (!file) {
+        errorCallback("File not found");
+        return;
+    }
+
+    //Performing another 2 checks based on the image file's type and size
+
+    const fileType = file.type;
+    const fileSize = file.size / 1024 / 1024;
+
+    if (!fileType.includes("image")) {
+        errorCallback("File must be an image");
+        return;
+    }
+
+    if (fileSize > 2) {
+        errorCallback("File must be smaller than 2MB");
+        return;
+    }
+
+    // Each Image has its reference in firebase's storage
+
+    const storageRef = ref(storage, `images/${file.name}`);
+
+    // Achieving the task of uploading an image
+
+    const task = uploadBytesResumable(storageRef, file);
+
+    // Applying the "on" listener on task which checks image "state_changed" with 3 different callbacks
+    // which are "snapshot","error",()
+    // snapshot is for checking the progress of uploading an image
+    // error is to show any errors while an image is being uploaded
+    // () is called when the whole image file is uploaded and we want to download the image from the storage
+
+    task.on('state_changed', (snapshot) => {
+        console.log(snapshot);
+
+        // finding the progress of Image Uploading in percentage
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        progressCallback(progress);
+    }, error => {
+        errorCallback(error.message);
+    }, () => {
+        getDownloadURL(storageRef).then(url => {
+            urlCallback(url);
+        });
+    });
+};
+
+
+export { app as default, auth, db, updateUserDatabase, getUserFromDatabase, uploadImage };
